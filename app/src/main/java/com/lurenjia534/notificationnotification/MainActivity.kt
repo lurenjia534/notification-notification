a27530b90afc472fbfbfaa2ded19dada3ca95a47
lurenjia534 <lurenjia534@outlook.com>
2024-01-16 12:43:08 +0800

    APP: init

---
+++ app/src/main/java/com/lurenjia534/notificationnotification/MainActivity.kt
@@ -0,0 +1,158 @@
+package com.lurenjia534.notificationnotification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import com.lurenjia534.notificationnotification.ui.theme.NotificationNotificationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NotificationNotificationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.primary
+                ) {
+                    AppUI()
+                }
+            }
+        }
+    }
+}
+
+fun sendNotification(context: Context, message: String) {
+    val notificationManager = ContextCompat.getSystemService(
+        context, NotificationManager::class.java
+    ) as NotificationManager
+
+    // 创建一个通知通道（在 Android Oreo 及以上版本中必须这样做）
+    val channelId = "Notice_Memo"
+    val channelName = "cno_Notice_Memo"
+    val channel = NotificationChannel(
+        channelId, channelName, NotificationManager.IMPORTANCE_HIGH
+    )
+    notificationManager.createNotificationChannel(channel)
+
+    // 构建通知
+    val notification = NotificationCompat.Builder(context, channelId)
+        .setContentTitle("Cno Notice Memo")
+        .setContentText(message)
+        .setSmallIcon(R.drawable.ic_launcher_foreground) // 设置通知图标
+        .setPriority(NotificationCompat.PRIORITY_HIGH) // 设置优先级
+        .setOngoing(true) // 使通知成为持久性通知
+        .build()
+
+    // 显示通知
+    notificationManager.notify(1001, notification)
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview
+fun AppUI(){
+
+
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+       Column {
+           OutlinedCard(modifier = Modifier
+               .width(380.dp)
+               .height(100.dp)) {
+               Row {
+                   Spacer(modifier = Modifier.width(45.dp))
+                   Text(text = "👏 Welcome back Cno!", fontSize = 25.sp, fontWeight = FontWeight.Bold, )
+               }
+               Spacer(modifier = Modifier.height(25.dp))
+               Row {
+                   Spacer(modifier = Modifier.width(75.dp))
+                   Text(text = "❤ Is everything okay? Right!", fontSize = 15.sp, fontWeight = FontWeight.Light, )
+               }
+           }
+           Spacer(modifier = Modifier.height(50.dp))
+           var text by remember { mutableStateOf(TextFieldValue("")) }
+           OutlinedCard(modifier = Modifier
+               .height(280.dp)
+               .width(380.dp), colors = CardDefaults.outlinedCardColors()) {
+               Spacer(modifier = Modifier.height(50.dp))
+               Row {
+                   Spacer(modifier = Modifier.width(50.dp))
+                   OutlinedTextField(value = text, onValueChange = { newText -> text = newText },
+                       label = { Text("notification")},
+                       leadingIcon = { Icon(imageVector = Icons.Default.Notifications, contentDescription = null) },
+                       colors = TextFieldDefaults.outlinedTextFieldColors(
+                           focusedBorderColor = Color.Gray, // 获得焦点时的边框颜色
+                           unfocusedBorderColor = Color.Gray // 未获得焦点时的边框颜色
+                       )
+                   )
+               }
+               Spacer(modifier = Modifier.height(50.dp))
+               Row() {
+                  Spacer(modifier = Modifier.width(130.dp))
+                   val context = LocalContext.current
+                   Button(onClick = {
+                       sendNotification(context,text.text)
+                   }) {
+                       Icon(imageVector = Icons.Default.Send, contentDescription = null)
+                       Spacer(modifier = Modifier
+                           .width(5.dp)
+                           .height(10.dp))
+                       Text(text = "Send!", fontWeight = FontWeight.Bold)
+                   }
+               }
+           }
+       }
+    }
+}
