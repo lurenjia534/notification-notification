c93aa8ec77d79ae276d1f7c3d087c8eb41d39cbd
lurenjia534 <lurenjia534@outlook.com>
2024-01-18 21:50:55 +0800

    App: Optimize import

--- app/src/main/java/com/lurenjia534/notificationnotification/MainActivity.kt
+++ app/src/main/java/com/lurenjia534/notificationnotification/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.lurenjia534.notificationnotification
 
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
