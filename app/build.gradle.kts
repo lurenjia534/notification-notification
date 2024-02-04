296376eaec5f4a6c8a9630bd0cf2e2ce1d5f0a72
lurenjia534 <lurenjia534@outlook.com>
2024-02-05 06:18:02 +0800

    App: Minor tweaks

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "1.2"
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
--- app/src/main/java/com/lurenjia534/notificationnotification/MainActivity.kt
+++ app/src/main/java/com/lurenjia534/notificationnotification/MainActivity.kt
@@ -60,7 +60,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.onBackground
+                    color = MaterialTheme.colorScheme.primary
                 ) {
                     AppUI()
                 }
