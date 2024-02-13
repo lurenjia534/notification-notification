9a6a8f7e15b1a4ea44c1ea77d0509a85b5700a0c
lurenjia534 <lurenjia534@outlook.com>
2024-02-13 23:58:53 +0800

    App: enable enableEdgeToEdge

--- app/src/main/java/com/lurenjia534/notificationnotification/MainActivity.kt
+++ app/src/main/java/com/lurenjia534/notificationnotification/MainActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -55,6 +56,7 @@ import com.lurenjia534.notificationnotification.ui.theme.NotificationNotificatio
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             NotificationNotificationTheme {
                 // A surface container using the 'background' color from the theme
